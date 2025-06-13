dbc3ca34f64138a53760d3def841da06559aee99
Clear Organ <brian.mcintosh96@gmail.com>
2025-06-13 17:43:37 -0400

    replaced for loop with forEach loop to iterate over the object in the pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,6 @@ let pokemonList = [
     {name: "Deino", height: 0.8, type: ["dark", "dragon"]}
 ];
 
-pokemonList.forEach ((let i = 0; i < pokemonList.length; i++) {   //checks if i, index of zero is greater than the whole array index, then adds 1.
-    let p = pokemonList[i];  //set p variable with the for loop index.
-    document.write("<p>" + p.name + " (Height " + p.height + ")")  // document.write writes to the html document using elements inside of strings.
-
-    if (p.height > 1.0) {
-        document.write(" - Wow, that's big!");
-    }
-
-    document.write("</p>")  //This closes the tag but also display the wow that's big next to the true conditional value.
-}
-
-
-
-
-
+pokemonList.forEach (function(details) {   //forEach function with funcion as parameter and name parameter as a placeholder to call the object.
+    console.log(details); //calls the parameter that containts the elements in the objects.
+})
