7d59a2c69f4ff40ad532a6f226be20861532ef73
Clear Organ <brian.mcintosh96@gmail.com>
2025-06-15 19:43:47 -0400

    Assigned IIFE to pokemonRepository, used the add() and getAll function inside of IIFE, used add(), getAll() to add pokemon and display output

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
-let pokemonList = [
-    {name: "Braviary", height: 1.5, type: ["flying", "normal"]},
-    {name: "Durant", height: 0.3, type: ["steel", "bug"]},
-    {name: "Deino", height: 0.8, type: ["dark", "dragon"]}
-];
+let pokemonRepository= (function () {
+    let pokemonList = [
+        {name: "Braviary", height: 1.5, type: ["flying", "normal"]},
+        {name: "Durant", height: 0.3, type: ["steel", "bug"]},
+        {name: "Deino", height: 0.8, type: ["dark", "dragon"]}
+    ];
 
-pokemonList.forEach (function(details) {   //forEach function with funcion as parameter and name parameter as a placeholder to call the object.
-    console.log(details); //calls the parameter that containts the elements in the objects.
+
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+
+        function getAll() {
+            return pokemonList
+        }
+
+    return {
+            add: add,
+            getAll: getAll
+    };
+
+})() // IIFE
+
+// Uses add method/function to add new pokemon to the pokemonRepository object from outside the IIFE.
+pokemonRepository.add({name: "Piakchu", height: 0.4, type: ["electric"]})
+
+//Use forEach loop to iterate over the all the property and values in the pokemonRepository object using the getAll() to access it outside of IIFE.
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write("<p>" + pokemon.name + " " + pokemon.height + " " + pokemon.type.join(", ") + "</p>")
 })
+
+// logs to console the iteration of pokemonRepository object using getALL() to access it outside of the IIFE.
+console.log(pokemonRepository.getAll())
+
